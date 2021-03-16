c62fb04438f4be8d2aaacab3dd235373c59fa6b6
Jmax <jmax407@gmail.com>
2021-03-15 20:40:24 -0400

    create login

---
+++ db/seeds.sql
@@ -0,0 +1,74 @@
+[
+    {
+        "id": 1,
+        "username": "sdalman0",
+        "email": "abrandi0@list-manage.com",
+        "password": "4k0uONtIwvy"
+    },
+    {
+        "id": 2,
+        "username": "dclemencet1",
+        "email": "jdowty1@tinyurl.com",
+        "password": "DwxuqTt"
+    },
+    {
+        "id": 3,
+        "username": "jsperski2",
+        "email": "mboswell2@1688.com",
+        "password": "Kad2f9Y7zS"
+    },
+    {
+        "id": 4,
+        "username": "slynnett3",
+        "email": "leales3@stumbleupon.com",
+        "password": "z1uQyVLn"
+    },
+    {
+        "id": 5,
+        "username": "lfawloe4",
+        "email": "alundy4@barnesandnoble.com",
+        "password": "mjHDp7sMg8w9"
+    },
+    {
+        "id": 6,
+        "username": "zwhewill5",
+        "email": "dpignon5@msn.com",
+        "password": "eJF8ElXBKLS"
+    },
+    {
+        "id": 7,
+        "username": "ojosephy6",
+        "email": "epelman6@slashdot.org",
+        "password": "NJ65BwxY7JQv"
+    },
+    {
+        "id": 8,
+        "username": "mpriel7",
+        "email": "etruss7@usatoday.com",
+        "password": "CnOPLdl"
+    },
+    {
+        "id": 9,
+        "username": "omoisey8",
+        "email": "bgenney8@si.edu",
+        "password": "7tAO4Li"
+    },
+    {
+        "id": 10,
+        "username": "mvandriel9",
+        "email": "lmacneish9@soundcloud.com",
+        "password": "ILluIUu"
+    },
+    {
+        "id": 11,
+        "username": "cdonetta",
+        "email": "rgerardina@rambler.ru",
+        "password": "J4sFH2"
+    },
+    {
+        "id": 12,
+        "username": "fdorberb",
+        "email": "mhuckeb@blogspot.com",
+        "password": "LkpQxkgY9T8L"
+    }
+]
